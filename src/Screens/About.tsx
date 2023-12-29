b2a6b64a431fbdbf4e0e3da87fed9a37ef507f45
Ravinder2401 <ravinder@baltech.in>
2023-12-29 17:03:44 +0530

    Example-3 using useState and useReducer

--- src/Screens/About.tsx
+++ src/Screens/About.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useReducer } from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamsList } from '../../App';
 
@@ -7,10 +7,35 @@ interface AboutScreenProps{
     navigation: StackNavigationProp<RootStackParamsList, 'About'>
 }
 
+type MyState={
+  count:number
+}
+
+const initialState:MyState={
+  count:0
+}
+
+type Action={
+  type:'Increment' | 'Decrement'
+}
+
+const reducer = (state:MyState,action:Action):MyState => {
+  switch(action.type) {
+    case 'Increment':
+      return {...state, count: state.count + 1};
+    case 'Decrement':
+      return {...state, count: state.count - 1};
+    default:
+      return state;
+  }
+}
+
 const About = ({navigation}:AboutScreenProps) => {
+  const [state,dispatch] = useReducer(reducer,initialState);
   return (
-    <View>
-      <Text>About</Text>
+    <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+      <Text>{state.count}</Text>
+      <Text onPress={()=>dispatch({type:'Increment'})} style={{padding:10,borderWidth:1,color:'black'}}>Change Count</Text>
     </View>
   )
 }
--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,16 +1,38 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamsList } from '../../App';
 
 interface HomeScreenProps{
     navigation: StackNavigationProp<RootStackParamsList, 'Home'>
 }
+
+interface User {
+  name: string,
+  age: number,
+  email:string
+}
+
+interface Items {
+  title: string,
+  price: number,
+  id: string
+}
+
 const Home = ({navigation}:HomeScreenProps) => {
+
+  // typescript state decleration
+  const [counter,setCounter] = useState<number>(0);
+  const [name,setName] = useState<string>('');
+  const [isLoaded,setIsLoaded] = useState<boolean>(false);
+  const [user,setUser] = useState<User>({name:'',age:0,email:''});
+  const [items,setItems] = useState<Items[]>([]);
+  const [details,setDetails] = useState<null>(null);
+  
   return (
     <View>
       <Text>Home</Text>
-      <Text onPress={()=>{
+      <Text style={{margin:20,backgroundColor:'lightgrey',padding:10,textAlign:'center'}} onPress={()=>{
         navigation.navigate("About")
       }}>Go to About</Text>
     </View>
